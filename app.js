f2f2220c0881ad174b17628321daea8a87a0b3b8
Pradeepkumar Theegala <pradeepkumartheegala@gmail.com>
2019-03-08 16:16:13 -0600

    Added new files and changed the original repo

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const hostname = '0.0.0.0'    // allows access from remote computers
 const port = 3002
 
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
+  res.sendFile(path.join(__dirname, './index.html'))
 })
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/big',  (req, res) =>{
+app.get('/big', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/greeting/:id',  (req, res) =>{
+app.get('/greeting/:id', (req, res) => {
   res.send('Hello! The id was ' + req.params.id)
 })
 
-app.get('/yo/:buddy',  (req, res) =>{
+app.get('/yo/:buddy', (req, res) => {
   res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
+})
+
+app.get('/:user/', (req, res) => {
+  res.send('<h1>Hello,' + req.params.user + '<h1><b><p>You entered name: ' + req.params.user)
+})
+
+app.get('/welcome/', (req, res) => {
+  res.send('<h1>Welcome to Web Application<h1>')
 })
 
 // handle non-existant routes
 app.use((req, res, next) => {
-  res.status(404).send('status 404 - that page was not found');
+  res.status(404).send('<h1 style="color:red text-align:center">status 404 - that page was not found<h1>');
 })
 
 app.listen(port, hostname, () => {
